7f61a576ecbcb5526f98bb290f9de4d12b661a83
Anahí Barajas <barajasperezanahi@gmail.com>
2022-11-17 22:57:23 -0600

    Agregar archivos de proyecto.

---
+++ HolaMundo/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace HolaMundo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnValidar = new System.Windows.Forms.Button();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtPwd = new System.Windows.Forms.TextBox();
+            this.txtPwdVal = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnValidar
+            // 
+            this.btnValidar.Location = new System.Drawing.Point(213, 170);
+            this.btnValidar.Name = "btnValidar";
+            this.btnValidar.Size = new System.Drawing.Size(97, 27);
+            this.btnValidar.TabIndex = 0;
+            this.btnValidar.Text = "Validar";
+            this.btnValidar.UseVisualStyleBackColor = true;
+            this.btnValidar.Click += new System.EventHandler(this.btnValidar_Click);
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(69, 79);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(64, 13);
+            this.lblPassword.TabIndex = 1;
+            this.lblPassword.Text = "Contraseña:";
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.Location = new System.Drawing.Point(139, 76);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.Size = new System.Drawing.Size(171, 20);
+            this.txtPwd.TabIndex = 2;
+            // 
+            // txtPwdVal
+            // 
+            this.txtPwdVal.Location = new System.Drawing.Point(139, 118);
+            this.txtPwdVal.Name = "txtPwdVal";
+            this.txtPwdVal.Size = new System.Drawing.Size(171, 20);
+            this.txtPwdVal.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Repetir contraseña:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 239);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPwdVal);
+            this.Controls.Add(this.txtPwd);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.btnValidar);
+            this.Name = "Form1";
+            this.Text = "Validación de Contraseña";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnValidar;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtPwd;
+        private System.Windows.Forms.TextBox txtPwdVal;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ HolaMundo/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace HolaMundo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // Al menos una letra mayúscula
+        // Al menos una letra minúscula
+        // Al menos un símbolo
+        // Al menos un número
+        // Al menos 6 caracteres
+
+        private void btnValidar_Click(object sender, EventArgs e)
+        {
+            if (Regex.IsMatch(txtPwd.Text, "^(?=.*\\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\\w\\d\\s:])([^\\s]){6,}$"))
+            {                
+                if(txtPwdVal.Text == txtPwd.Text )
+                {
+                    MessageBox.Show("La contraseña ha sido validada");
+                }
+                else
+                {
+                    MessageBox.Show("Las contraseñas no son iguales");
+                }
+            }
+            else {
+                MessageBox.Show("La contraseña no contiene el formato válido");
+            }
+        }
+       
+    }
+}
+
